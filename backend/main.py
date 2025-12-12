3d9aff5392ff25e1ce5a4618ac6551894312dd3b
rob-cost <robertocostantino3@gmail.com>
2025-12-12 12:22:47 +0100

    Improved frontend: add reference track button upload

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, UploadFile, File, Request
+from fastapi import FastAPI, UploadFile, File, Request, HTTPException
+from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+import asyncio
 
 
-from analysis.llm.report_generator import generate_report
+from analysis.llm.audio_analysis_generator import generate_report
 from analysis.utils.file_upload import process_file
 
 
@@ -24,34 +26,46 @@ app.add_middleware(
 
 # --- ENDPOINTS ---
 
-# @app.post("/analyze")
-# @limiter.limit("5/minute") # Limit amount of request per IP
-# async def analyze(request: Request, file:UploadFile = File(...)):
-#     return await process_file(file)
-
-# @app.post("/analyze_reference")
-# @limiter.limit("5/minute")
-# async def analyze_reference(request: Request, file:UploadFile = File(...)):
-#     return await process_file(file)
-
-# @app.post("/generate_report")
-# async def report_generator(payload: dict):
-#     return await generate_report(payload)
-
 @app.post("/analyze_and_report")
 @limiter.limit("5/minute") # Limit amount of request per IP
-async def analyze_and_report(request: Request, file:UploadFile = File(...)):
-    try:
-        features = await process_file(file)
-    except Exception as e:
-        print(f"Error in generating features: {e}")
+async def analyze_and_report(
+    request: Request, 
+    track_file:UploadFile = File(...),
+    reference_file:Optional[UploadFile]=File(None)
+    ):
 
+    features = None
+    ref_features = None
+    report = None
+
+    # Analyzing main track and reference if provided
+    if reference_file is None:
+        try:
+            features = await process_file(track_file)
+        except Exception as e:
+            print(f"Error in generating features: {e}")
+            raise HTTPException(status_code=500, detail="Error processing the main track file.")
+    else:
+        try:
+            features, ref_features = await asyncio.gather(
+                process_file(track_file),
+                process_file(reference_file)
+            )
+        except Exception as e:
+            print(f"Error in generating features: {e}")
+            raise HTTPException(status_code=500, detail="Error processing one of the track files.")
+
+
+    # Generate AI Report
     try:
-        report = generate_report(features)
+        print("GENERATING AI REPORT")
+        report = generate_report(features, ref_features)
     except Exception as e:
         print(f"Error in generating a report: {e}")
+        raise HTTPException(status_code=500, detail="Error generating the analysis report.")
 
     return {
         "features": features,
+        "ref_features": ref_features,
         "report": report
     }
